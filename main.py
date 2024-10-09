f52765083470edee1b9057f47fb93b7ecc999123
IIdelightedII <tastewatermelon105@gmail.com>
2024-10-09 17:53:19 +0500

    added random words, modified messages and optimized

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 import os
-
+import random
 print("Привет! Я загадал слово, твоя задача угадать его по буквам.")
 input("Нажмите Enter чтобы продолжить")
 os.system("cls")
 print("Поехали!")
+words = ["тумба", "компьютер", "камера", "клавиатура", "линейка"]
 
-
-word = "дистрибутив"
+word = random.choice(words)
 attempts = 10
 letters = []
 guessed_letters = 0
 while attempts > 0:
+    guessed_letters = 0
     print(f"твоё количество попыток: {attempts}")
 
     for i in range(len(word)):
         if word[i] in letters:
             print(word[i],  end =" ")
             guessed_letters += 1
-            if guessed_letters == len(word):
-                os.system("cls")
-                print(f"Поздравляю, ты отгадал слово!: \n{word}")
-                exit()
-
         else:
             print("*", end=" ")
 
-    letters.append(input("\nВведи букву: "))
+    if guessed_letters == len(word):
+        os.system("cls")
+        print(f"Поздравляю, ты отгадал слово! \n{word}")
+        exit()
+    letter = input("\nВведи букву: ").lower()
     os.system("cls")
-
-    while letters[-1] in letters[0:-1]:
-        del letters[-1]
-        letters.append(input(f"кажется, ты уже вводил эту букву, введи другую. Вот все твои буквы: {letters}: "))
+    if letter in letters:
+        print(f"Вот все твои буквы: {letters}.\nТы уже вводил букву \"{letter}\"")
+        continue
+    letters.append(letter)
 
     if not letters[-1] in word:
         attempts -= 1
-    guessed_letters = 0
 
-print("К сожалению ты не отгадал слово")
+print(f"К сожалению ты не отгадал слово - {word}")
